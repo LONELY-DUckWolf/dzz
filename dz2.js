6f613725cf88761cda013ee5579ae767897ed691
lonely <boryslavshevchenko18112011@gmail.com>
2024-10-01 22:33:48 +0300

    happy happy happy nyanyanyanyanyanyanya

---
+++ dz2.js
@@ -0,0 +1,41 @@
+// let celsius = 25;
+// let fahrenheit = (celsius * 9/5) + 32;
+// console.log(fahrenheit + "°F");
+
+
+// let daysInMonth = 30; 
+// let hoursInMonth = daysInMonth * 24;
+// let minutesInMonth = hoursInMonth * 60;
+// console.log(hoursInMonth + " годин у місяці");
+// console.log(minutesInMonth + " хвилин у місяці");
+
+
+// let health = 100;
+// let energy = 80; 
+// health -= 20; 
+// energy -= 30; 
+// console.log("Здоров'я: " + health);
+// console.log("Енергія: " + energy);
+
+
+// let totalPurchase = 500;
+// let discount = 0.1; 
+// let discountedPrice = totalPurchase * (1 - discount);
+// console.log("Сума після знижки: " + discountedPrice);
+
+
+// let floatNumber = 5.89;
+// let roundedNumber = Math.floor(floatNumber);
+// console.log("Округлене число: " + roundedNumber);
+
+
+// let floatString = "45.67";
+// let floatResult = parseFloat(floatString);
+// console.log("Перетворене число: " + floatResult);
+
+
+// let integerString = "123";
+// let intResult = parseInt(integerString);
+// console.log("Перетворене ціле число: " + intResult);
+
+
---
+++ dz3.js
@@ -0,0 +1,19 @@
+// const result = 5 + 5 + 'Оля 24 сентебря Тима 9 липня';
+// console.log(result);
+
+// const email = 'cs.shad3096@gmail.com';
+// console.log(email)
+// console.log(email.length);
+// const checkEmail = email.includes('@');
+// console.log(checkEmail);
+
+// const pidmet = 'I'
+// const glogol = 'am'
+// const name = 'Grut'
+// const fullName = `${pidmet} ${glogol} ${name}`
+// console.log(fullName);
+
+// let userName = prompt("Введіть своє ім'я: ");
+// alert("Дякуємо, " + userName + "!. До сплати 20 гривень! Ось моя карта (4441 1144 3317 5179)!");
+
+
