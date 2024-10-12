dba887738fe055a08c4991f227d5665f85059d72
lonely <boryslavshevchenko18112011@gmail.com>
2024-10-13 00:27:55 +0300

    ,ttttttttttt

--- dz4.js
+++ dz4.js
@@ -1,73 +1,73 @@
-const isSubscribe1 = prompt("mono")
-const isSubscribe2 = prompt("bank")
-const acces = isSubscribe1 === 'mono' && isSubscribe2 === 'bank';
-console.log(isSubscribe1)
-console.log(isSubscribe2)
-if (acces) {
-    alert("ok")
-} else {
-    alert("ne ok")
-}
+// const isSubscribe1 = prompt("mono")
+// const isSubscribe2 = prompt("bank")
+// const acces = isSubscribe1 === 'mono' && isSubscribe2 === 'bank';
+// console.log(isSubscribe1)
+// console.log(isSubscribe2)
+// if (acces) {
+//     alert("ok")
+// } else {
+//     alert("ne ok")
+// }
 
 
-const nummber1 = prompt("больше 10 надо")
-const nummber2 = prompt("больше 10 надо")
-const acces1 = nummber1 > 10 && nummber2 > 10;
-console.log(nummber1)
-console.log(nummber2)
-if (acces1) {
-    alert("ok")
-} else {
-    alert("ne ok")
-}
+// const nummber1 = prompt("больше 10 надо")
+// const nummber2 = prompt("больше 10 надо")
+// const acces1 = nummber1 > 10 && nummber2 > 10;
+// console.log(nummber1)
+// console.log(nummber2)
+// if (acces1) {
+//     alert("ok")
+// } else {
+//     alert("ne ok")
+// }
 
 
-const isCards = prompt("I can do a trick with cards")
-const acces2 = isCards === 'cards';
-console.log(isCards)
-if (acces2) {
-    alert("gi")
-} else {
-    alert("ne gi")
-}
+// const isCards = prompt("I can do a trick with cards")
+// const acces2 = isCards === 'cards';
+// console.log(isCards)
+// if (acces2) {
+//     alert("gi")
+// } else {
+//     alert("ne gi")
+// }
 
 
-const isBiger = prompt("10 <= x <= 20")
-const acces3 = isBiger >= 10 && isBiger <= 20;
-console.log(isBiger)
-if (acces3) {
-    alert("gi")
-} else {
-    alert("ne gi")
-}
+// const isBiger = prompt("10 <= x <= 20")
+// const acces3 = isBiger >= 10 && isBiger <= 20;
+// console.log(isBiger)
+// if (acces3) {
+//     alert("gi")
+// } else {
+//     alert("ne gi")
+// }
 
 
-const isName = prompt('name');
-const isEmail = prompt('email');
-const isPassword = prompt('password');
-console.log(isName);
+// const isName = prompt('name');
+// const isEmail = prompt('email');
+// const isPassword = prompt('password');
+// console.log(isName);
 
 
-if (isName.length > 3) {
-    alert('oka')
+// if (isName.length > 3) {
+//     alert('oka')
 
-} else {
-    alert('nouuu')
-}
+// } else {
+//     alert('nouuu')
+// }
 
 
-console.log(isEmail);
-if (isEmail === '@gmail.com' || isEmail === '@ukr.net' || isEmail === '@hotmail.com') {
-    alert('oka')
+// console.log(isEmail);
+// if (isEmail === '@gmail.com' || isEmail === '@ukr.net' || isEmail === '@hotmail.com') {
+//     alert('oka')
 
-} else {
-    alert('nouuu')
-}
+// } else {
+//     alert('nouuu')
+// }
 
-console.log(isPassword);
-if (isPassword.length > 6) {
-    alert('oka')
+// console.log(isPassword);
+// if (isPassword.length > 6) {
+//     alert('oka')
 
-} else {
-    alert('nouuu')
-}
+// } else {
+//     alert('nouuu')
+// }
---
+++ dz5.js
@@ -0,0 +1,125 @@
+let select = prompt("что вы желаете? шиншилу, трава, фрукты")
+let caseCoffee = select.toLocaleLowerCase()
+
+switch (caseCoffee) {
+    case "шиншилу":
+        alert("Типо капучино");
+
+        break;
+
+    case "трава":
+        alert("не чай с имберьом");
+
+        break;
+
+    case "фрукты":
+        alert("сок фруктовый");
+
+        break;
+
+    default:
+        alert("бери что хочешь точно пожелеешь ой не пожелеешь")
+}
+
+
+let day = prompt("день недели")
+let dayWitch = day.toLocaleLowerCase()
+
+switch (dayWitch) {
+    case "вторник":
+    case "понедельник":
+    case "пятница":
+    case "среда":
+    case "четверг":
+        alert("Учоба или робота")
+        break
+
+    case "Софт скилс":
+    case "Тех скилс":
+        alert("опять учиться");
+
+        break;
+    default:
+        alert("дня нету")
+}
+
+let month = prompt("ведите любой год месяца")
+let monthWitch = month.toLocaleLowerCase()
+
+switch (month) {
+    case "декабрь":
+    case "февраль":
+    case "январь":
+        alert("зима");
+
+        break;
+    case "март":
+    case "апрель":
+    case "май":
+        alert("весна");
+
+        break;
+    case "июнь":
+    case "июль":
+    case "август":
+        alert("лето");
+
+        break;
+    case "сентебрь":
+    case "октябрь":
+    case "ноябрь":
+        alert("осень")
+
+        break;
+    default:
+        alert("месяц какой")
+}
+
+let number = prompt("1 до 12")
+
+switch (number) {
+    case "1":
+    case "5":
+    case "7":
+    case "10":
+    case "12":
+        alert("31 day");
+
+        break;
+    case "4":
+    case "8":
+    case "9":
+        alert("30 day");
+
+        break;
+    case "2":
+    case "3":
+    case "6":
+    case "11":
+        alert("29 day");
+
+        break;
+    default:
+        alert("какой день алоо")
+}
+
+let color = prompt("не скажешь красный либо зелёный я тебя по айпи вычислю ")
+let xz = color.toLocaleLowerCase()
+
+switch (xz) {
+    case "зелёный":
+        alert("ок");
+
+        break;
+    case "желтый":
+        alert("готов");
+
+        break;
+    case "красный":
+        alert("асталависта бейби")
+
+        break;
+    default:
+        alert("говори зелёный")
+}
+
