8d161a198f2f8d7bd73093501db8075ef1ff313e
lonely <boryslavshevchenko18112011@gmail.com>
2024-10-19 23:44:24 +0300

    uzas

--- dz5.js
+++ dz5.js
@@ -1,125 +1,125 @@
-let select = prompt("что вы желаете? шиншилу, трава, фрукты")
-let caseCoffee = select
+// let select = prompt("что вы желаете? шиншилу, трава, фрукты")
+// let caseCoffee = select
 
-switch (caseCoffee) {
-    case "шиншилу":
-        alert("Типо капучино");
+// switch (caseCoffee) {
+//     case "шиншилу":
+//         alert("Типо капучино");
 
-        break;
+//         break;
 
-    case "трава":
-        alert("не чай с имберьом");
+//     case "трава":
+//         alert("не чай с имберьом");
 
-        break;
+//         break;
 
-    case "фрукты":
-        alert("сок фруктовый");
+//     case "фрукты":
+//         alert("сок фруктовый");
 
-        break;
+//         break;
 
-    default:
-        alert("бери что хочешь точно пожелеешь ой не пожелеешь")
-}
+//     default:
+//         alert("бери что хочешь точно пожелеешь ой не пожелеешь")
+// }
 
 
-let day = prompt("день недели")
-let dayWitch = day
+// let day = prompt("день недели")
+// let dayWitch = day
 
-switch (dayWitch) {
-    case "вторник":
-    case "понедельник":
-    case "пятница":
-    case "среда":
-    case "четверг":
-        alert("Учоба или робота")
-        break
+// switch (dayWitch) {
+//     case "вторник":
+//     case "понедельник":
+//     case "пятница":рр
+//     case "среда":
+//     case "четверг":
+//         alert("Учоба или робота")
+//         break
 
-    case "Софт скилс":
-    case "Тех скилс":
-        alert("опять учиться");
+//     case "Софт скилс":
+//     case "Тех скилс":
+//         alert("опять учиться");
 
-        break;
-    default:
-        alert("дня нету")
-}
+//         break;
+//     default:
+//         alert("дня нету")
+// }
 
-let month = prompt("ведите любой год месяца")
-let monthWitch = month
+// let month = prompt("ведите любой год месяца")
+// let monthWitch = month
 
-switch (month) {
-    case "декабрь":
-    case "февраль":
-    case "январь":
-        alert("зима");
+// switch (month) {
+//     case "декабрь":
+//     case "февраль":
+//     case "январь":
+//         alert("зима");
 
-        break;
-    case "март":
-    case "апрель":
-    case "май":
-        alert("весна");
+//         break;
+//     case "март":
+//     case "апрель":
+//     case "май":
+//         alert("весна");
 
-        break;
-    case "июнь":
-    case "июль":
-    case "август":
-        alert("лето");
+//         break;
+//     case "июнь":
+//     case "июль":
+//     case "август":
+//         alert("лето");
 
-        break;
-    case "сентебрь":
-    case "октябрь":
-    case "ноябрь":
-        alert("осень")
+//         break;
+//     case "сентебрь":
+//     case "октябрь":
+//     case "ноябрь":
+//         alert("осень")
 
-        break;
-    default:
-        alert("месяц какой")
-}
+//         break;
+//     default:
+//         alert("месяц какой")
+// }
 
-let number = prompt("1 до 12")
+// let number = prompt("1 до 12")
 
-switch (number) {
-    case "1":
-    case "5":
-    case "7":
-    case "10":
-    case "12":
-        alert("31 day");
+// switch (number) {
+//     case "1":
+//     case "5":
+//     case "7":
+//     case "10":
+//     case "12":
+//         alert("31 day");
 
-        break;
-    case "4":
-    case "8":
-    case "9":
-        alert("30 day");
+//         break;
+//     case "4":
+//     case "8":
+//     case "9":
+//         alert("30 day");
 
-        break;
-    case "2":
-    case "3":
-    case "6":
-    case "11":
-        alert("29 day");
+//         break;
+//     case "2":
+//     case "3":
+//     case "6":
+//     case "11":
+//         alert("29 day");
 
-        break;
-    default:
-        alert("какой день алоо")
-}
+//         break;
+//     default:
+//         alert("какой день алоо")
+// }
 
-let color = prompt("не скажешь красный либо зелёный я тебя по айпи вычислю ")
-let xz = color
+// let color = prompt("не скажешь красный либо зелёный я тебя по айпи вычислю ")
+// let xz = color
 
-switch (xz) {
-    case "зелёный":
-        alert("ок");
+// switch (xz) {
+//     case "зелёный":
+//         alert("ок");
 
-        break;
-    case "желтый":
-        alert("готов");
+//         break;
+//     case "желтый":
+//         alert("готов");
 
-        break;
-    case "красный":
-        alert("асталависта бейби")
+//         break;
+//     case "красный":
+//         alert("асталависта бейби")
 
-        break;
-    default:
-        alert("говори зелёный")
-}
+//         break;
+//     default:
+//         alert("говори зелёный")
+// }
 
