6fd9fc5aab6f1ac315879e79c2352bc0ed81863f
lonely <boryslavshevchenko18112011@gmail.com>
2025-01-11 22:32:52 +0200

    fwfwff

--- 15.js
+++ 15.js
@@ -1,24 +1,71 @@
-const players = [
-    { name: 'Mango', colorEye: "white", friends: 1, online: false, gender: "man", balance: "1"},
-    { name: 'Poly', colorEye: "blue", friends: 23, online: true, gender: "cat", balance: "2" },
-    { name: 'Kiwi', colorEye: "yellow", friends: 66, online: true, gender: "man", balance: "5" },
-    { name: 'Ajax', colorEye: "red", friends: 5, online: false, gender: "cat", balance: "235" },
-    { name: 'Chelsy', colorEye: "black", friends: 2, online: true, gender: "man", balance: "5325" },
-]
 
 //1
 
-const allDenga = players.reduce((rap, player) => {
-    return rap + player.balance;
-  }, 0);
-  console.log("");
-  console.log(allDenga);
+
+
+const w = [23, 32, 3, 32, 3, 2];
+
+const sum = w.reduce((totalSum, number) => {
+    return totalSum + number
+
+}, 0)
+
+console.log(sum);
+
+//2
+
+const players = [
+    { name: 'player-1', fname: 'Mango', },
+    { name: 'player-2', fname: 'Poly', },
+    { name: 'player-3', fname: 'Kiwi', },
+    { name: 'player-4', fname: 'Ajax', },
+    { name: 'player-5', fname: 'Chelsy', },
+  ]
+
+  const friendsName = (players, fName) => {
+    return players.reduce((rap, { name, fname }) => {
+      if (fname.includes(fName)) {
+        rap.push(name);
+      }
+      return rap;
+    }, []);
+  };
+
+  console.log(friendsName(players, 'Mango'))
+
 
 //3
 const playersFriends = (players, min, max) =>
-players.filter((player) => player.friends >= min && player.friends <= max);
+players.reduce((player) => player.friends >= min && player.friends <= max);
 
-const min = 1;
-const max = 66;
+const mins = 1;
+const maxs = 66;
 
-console.log(playersFriends(players, min, max));
+console.log(playersFriends(players, mins, maxs));
+
+//4
+
+const abus = [
+    { name: 'player-1', skills: ['Mango',  'unity'] , },
+    { name: 'player-2', skills: ['Poly',   'blender'], },
+    { name: 'player-3', skills: ['Kiwi',   'blender'],},
+    { name: 'player-4', skills: ['Ajax',   'montaj'], },
+    { name: 'player-5', skills: ['Chelsy', 'shinomontaj'], },
+  ]
+
+
+
+  const alesSkills = abus.reduce((kap, abu) => {
+    abu.skills.forEach((skill) => {
+      if (!kap.includes(skill)) {
+        kap.push(skill);
+      }
+    });
+    return kap;
+  }, []);
+  
+  console.log(
+    alesSkills.sort((prevName, nextName) => {
+      return prevName.localeCompare(nextName);
+    })
+  );
